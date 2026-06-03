c7bfeb57324e251fb437bbaebe4f80ca2d096630
Rodrigo da Cruz Vieira <cvieira.rodrigo@gmail.com>
2026-06-03 09:57:25 -0300

    Compacta layout de dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,14 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('gerador-da-sorte');
+  private readonly document = inject(DOCUMENT);
+
+  protected toggleFullscreen(): void {
+    if (this.document.fullscreenElement) {
+      void this.document.exitFullscreen();
+      return;
+    }
+
+    void this.document.documentElement.requestFullscreen();
+  }
 }
